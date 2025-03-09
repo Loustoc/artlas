f532d468c4664fd4b419ba526dc838daceff2d2e
Lou Kail <84414167+Loustoc@users.noreply.github.com>
2025-03-09 16:54:34 +0100

    fix: removed top-level await

--- src/webgl.ts
+++ src/webgl.ts
@@ -12,9 +12,9 @@ import GrassVertexShader from "./assets/webgl/grass/vertex.vert?raw";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 const grassModel =
   "//cdn.wtlstudio.com/sample.wtlstudio.com/a776537a-3038-4cd0-a90a-dab044a3f7ec.glb";
-const gltf = await new GLTFLoader().loadAsync(grassModel);
 
-const initScene = (canvasEl: HTMLCanvasElement) => {
+const initScene = async (canvasEl: HTMLCanvasElement) => {
+  const gltf = await new GLTFLoader().loadAsync(grassModel);
   const raycaster = new THREE.Raycaster();
   const pane = new Pane();
   const PARAMS = {
